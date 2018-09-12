f71fca812ad69e9678f999365b30c819832a1a0e
Duyet Nguyen <duyet.vn@gmail.com>
2018-09-12 18:33:26 +0000

    currency

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
-gem 'sinatra'
-gem 'haml'
+source 'https://rubygems.org'
 
+gem 'sinatra', '~> 1.4', '>= 1.4.7'
+gem 'haml'
+gem 'json'
+gem 'bootstrap', '~> 4.1', '>= 4.1.3'
+gem 'pry'
--- app.rb
+++ app.rb
@@ -1,6 +1,29 @@
 require 'sinatra'
-
+require 'net/http'
+require 'json'
+require 'pry'
 
 get '/' do
-  haml :readme
-end 
+  base = params[:base] || "USD"
+  url = "https://api.exchangeratesapi.io/latest?base=#{base}"
+  @currencies = %w(USD EUR GBP)
+  target = get_data_from url
+  @rates = @target["rates"]
+
+  if request.xhr?
+    format.json {
+      render json:{
+        rates: @rates
+      }
+    }
+  end
+  haml :index
+
+end
+
+def get_data_from url
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+
+  JSON.parse(response)
+end
